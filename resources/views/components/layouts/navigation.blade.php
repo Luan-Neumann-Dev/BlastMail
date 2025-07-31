beab6d27b7c124e071ba6612a43bfc38b513aa81
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-07-30 21:28:05 -0300

    feat: email list view, controller and model

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -11,9 +11,17 @@
                 </div>
 
                 <!-- Navigation Links -->
+                <!-- Dashboard -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
+
+                <!-- Email List -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('email-list.index')" :active="request()->routeIs('email-list.*')">
+                        {{ __('Email List') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -67,8 +75,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            <!-- Dashboard -->
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <!-- Email List -->
+            <x-responsive-nav-link :href="route('email-list.index')" :active="request()->routeIs('email-list.*')">
+                {{ __('Email List') }}
             </x-responsive-nav-link>
         </div>
 
